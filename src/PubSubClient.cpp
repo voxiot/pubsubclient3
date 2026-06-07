b84c95f277bfc583631fedabfc489590a25219f8
Holger Müller <github@euhm.de>
2026-06-07 22:27:23 +0200

    Revert "fix indent"
    This reverts commit de5c95098ba05c4e5fa18f2c5762972513de672a.

--- src/PubSubClient.cpp
+++ src/PubSubClient.cpp
@@ -380,7 +380,7 @@ bool PubSubClient::handlePacket(uint8_t hdrLen, size_t length) {
                 uint16_t topicLen = (_buffer[hdrLen + 1] << 8) + _buffer[hdrLen + 2];  // topic length in bytes
                 char* topic = (char*)(_buffer + hdrLen + 3 - 1);                       // set the topic in the LSB of the topic lenght, as we move it there
                 size_t payloadOffset = hdrLen + 3ul + topicLen;  // payload starts after header and topic (if there is no packet identifier)
-                size_t payloadLen = length - payloadOffset;      // this might change by 2 if we have a QoS 1 or 2 message
+                size_t payloadLen = length - payloadOffset;            // this might change by 2 if we have a QoS 1 or 2 message
                 uint8_t* payload = _buffer + payloadOffset;
 
                 // Guard 2: ensure topic and payload fit in buffer
